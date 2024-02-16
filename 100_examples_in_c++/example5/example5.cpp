e47e1ccbcf483108562529a0295df96a84625566
Constantin Galus <ostapx79@gmail.com>
2024-02-16 19:08:03 +0300

    add: добавлен файл example5/example5.cpp

---
+++ 100_examples_in_c++/example5/example5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Размер char: " << sizeof(char) << " byte\n";
+    std::cout << "Размер int: " << sizeof(int) << " byte\n";
+    std::cout << "Размер float: " << sizeof(float) << " byte\n";
+    std::cout << "Размер double: " << sizeof(double) << " byte\n";
+    return 0;
+}
